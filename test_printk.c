05ad2486eaa8ceae5ea408df4f3319e8fa046522
mbykovcom <32464530+mbykovcom@users.noreply.github.com>
2020-11-14 11:31:47 +0300

    Update test_printk.c

--- test_printk.c
+++ test_printk.c
@@ -2,14 +2,15 @@
 #include <linux/module.h>
 #include "test_used.h"
 
-MODULE_LICENSE("GPL")
-static int __init test_printk_init ( void) {
-    printk(KERN_INFO "primary module inserted ∖n");
-    printk(KERN_INFO "param from used module : %s ∖n" , data_from_param);
-    return 0;
+MODULE_LICENSE("GPL");
+
+static int __init test_printk_init(void) {
+	printk(KERN_INFO "primary module inserted\n");
+	printk(KERN_INFO "param from used module: %s\n", data_from_param);
+	return 0;
 }
-static void __exit test_printk_exit ( void) {
-    printk(KERN_INFO "primary module removed∖n");
+static void __exit test_printk_exit(void) {
+	printk(KERN_INFO "primary module removed\n");
 }
 
 module_init(test_printk_init);
