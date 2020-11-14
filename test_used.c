56924470807f09c6f59111f094d52ff05aa38b1d
mbykovcom <32464530+mbykovcom@users.noreply.github.com>
2020-11-14 11:32:24 +0300

    Update test_used.c

--- test_used.c
+++ test_used.c
@@ -4,25 +4,25 @@
 #include <linux/string.h>
 #include "test_used.h"
 
-MODULE_LICENSE("GPL")
+MODULE_LICENSE("GPL");
 
 char * data_from_param;
-module_param(data_from_param, char p, 0);
+module_param(data_from_param, charp, 0);
 
 extern char * print_from_used(void) {
-    return data_from_param;
-    }
+	return data_from_param;
+}
 
 EXPORT_SYMBOL(data_from_param);
 
-static int __init test_used_init (void) {
-    printk (KERN_INFO "used module inserted with param : %s ∖n" , data_from_param);
-    return 0;
-    }
-
-static void __exit test_used_exit (void) {
-    printk (KERN_INFO "used module removed∖n");
-    }
+static int __init test_used_init(void) {
+	printk(KERN_INFO "used module inserted with param: %s\n",
+	       data_from_param);
+	return 0;
+}
+static void __exit test_used_exit(void) {
+	printk(KERN_INFO "used module removed\n");
+}
 
 module_init(test_used_init);
 module_exit(test_used_exit);
